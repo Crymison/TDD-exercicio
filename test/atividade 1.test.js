cb2b0a36b7e7cc53c9cd3dcf23f66b3e1b67bb70
Crymison <gabrielpaleta@hotmail.com>
2021-10-12 17:50:48 -0300

    primeiro commit

---
+++ test/atividade 1.test.js
@@ -0,0 +1,27 @@
+describe('Atividade 3 ex1', () =>{
+    test('Verificar Salario desenvolvedor >= 3.000,00', () => {
+
+        let nome = "Jorel";
+        let email = "jorel@gmail.com";
+        let salariobase = 3000;
+        let cargo = "Desenvolvedor"
+
+        const res = CalculadoradeFuncionarios(nome, email, salariobase, cargo);
+        
+        expect(res.cargo).toBe("Desenvolvedor senior");
+        expect(res.verifica).toBe(true);
+    });
+
+    test('Verificar Salario desenvolvedor < 3.000,00', () => {
+        
+        let nome = "Jorel";
+        let email = "jorel@gmail.com";
+        let salariobase = 2000;
+        let cargo = "Desenvolvedor"
+        
+        const res = CalculadoradeFuncionarios(nome, email, salariobase, cargo);
+        
+        expect(res.cargo).toBe("Desenvolvedor junior");
+        expect(res.verifica).toBe(false);
+    });
+});
