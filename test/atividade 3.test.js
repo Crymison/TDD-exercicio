3fa78aac1a2e3e30dc1d5c3fdded1cccf3070166
Crymison <gabrielpaleta@hotmail.com>
2021-10-12 18:11:37 -0300

    terceiro commit

--- test/atividade 3.test.js
+++ test/atividade 3.test.js
@@ -24,4 +24,26 @@ describe('Atividade 3 ex1', () =>{
         expect(res.cargo).toBe("Desenvolvedor junior");
         expect(res.verifica).toBe(false);
     });
+
+    test('Verificar Salario DBA >= 2.000,00', () => {
+        let nome = "Maria";
+        let email = "Maria@gmail.com";
+        let salariobase = 3500;
+        let cargo = "DBA"
+        
+        const res = CalculadoradeFuncionarios(nome, email, salariobase, cargo);
+        
+        expect(res.cargo).toBe("DBA senior");
+    });
+
+    test('Verificar Salario DBA < 2.000,00', () => {
+        let nome = "Gustavo";
+        let email = "gustavo@gmail.com";
+        let salariobase = 1600;
+        let cargo = "DBA"
+        
+        const res = CalculadoradeFuncionarios(nome, email, salariobase, cargo);
+        
+        expect(res.cargo).toBe("DBA junior");
+    });
 });
